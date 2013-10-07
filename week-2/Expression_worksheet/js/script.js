39213cc65327d45889a1c8e2d42703d6aab544bf
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-07 10:23:44 -0400

    Expressions worksheet commit1
    coments for dog years example.

---
+++ week-2/Expression_worksheet/js/script.js
@@ -0,0 +1,9 @@
+// Expressions Worksheet Tim ONeal October 7, 2013;
+
+// Dog years Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in Òdog years.Ó  Calculate how old Sparky the pit bull is in dog years based on his actual age.;
+
+// calculate sparky the pit bulls age in dog years.;
+
+// multiply dogYears by humanYears.;
+
+// Print out the output sparkyAge
