1146dbc92a28b98f97b58f6551cdfb0416e089fb
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-07 10:50:12 -0400

    Expressions Worksheet commit2
    Dog years exercise completed

--- week-2/Expression_worksheet/js/script.js
+++ week-2/Expression_worksheet/js/script.js
@@ -4,6 +4,14 @@
 
 // calculate sparky the pit bulls age in dog years.;
 
+var humanYears= 5;
+
+const dogYears= 7;
+
 // multiply dogYears by humanYears.;
 
-// Print out the output sparkyAge
+var sparkyAge= dogYears * humanYears;
+
+// Print out the output sparkyAge.;
+
+console.log("The age of Sparky the Pit Bull in dog years is " + sparkyAge)
