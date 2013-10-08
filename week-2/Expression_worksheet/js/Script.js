17ce2db71763221f95258a94351c81b34dfcc3aa
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-07 20:35:54 -0400

    dog years
    comments added for dog years exercise

--- week-2/Expression_worksheet/js/Script.js
+++ week-2/Expression_worksheet/js/Script.js
@@ -1,2 +1,9 @@
 // Expressions Worksheet Tim ONeal October 7, 2013;
 
+    /* Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7  years old in dog years. Calculate how old Sparky the pit bull is in dog years based on his actual age.*/
+    
+// Find Sparky the pit bulls age.;
+
+// Calculate Sparkys age in dog years dogYears*humanAge;
+
+//print out for sparkyAge;
