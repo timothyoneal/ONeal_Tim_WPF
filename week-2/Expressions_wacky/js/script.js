b81270edc339e5323ea9c04a924a8f1f195418b2
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-09 16:39:56 -0400

    expressions wacky commit 7
    added comment and semi colons that were missing
    // variables and constants;

--- week-2/Expressions_wacky/js/script.js
+++ week-2/Expressions_wacky/js/script.js
@@ -2,9 +2,10 @@
 
 /*This project will track the number of 2 point, 3 point and Free throws  basketball player makes in an individual woukout. As well as the percentage for all 3 and overall. Givens are there will be 50 shots at each range */
 
-var userName = prompt("What is your name?")     
+// variables and constants;
+var userName = prompt("What is your name?");     
 
-alert("Hello " + userName + " before you proceed further please take 50 shots from inside and outside of the the three point arch, and 50 free throws.")
+alert("Hello " + userName + " before you proceed further please take 50 shots from inside and outside of the the three point arch, and 50 free throws.");
 
 
 
