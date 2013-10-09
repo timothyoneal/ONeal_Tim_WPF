8ab542b7c81b9f263d7d7dc6808f5223ade11571
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-09 16:25:41 -0400

    expressions wacky commit 4
    added discription comment.
    /*This project will track the number of 2 point, 3 point and Free
    throws  basketball player makes in an individual woukout. As well as
    the percentage for all 3 and overall. Givens are there will be 50 shots
    at each range */

--- week-2/Expressions_wacky/js/script.js
+++ week-2/Expressions_wacky/js/script.js
@@ -1,3 +1,8 @@
 // Tim ONeal expressions wacky October 9 2013;
 
-alert("test")
+/*This project will track the number of 2 point, 3 point and Free throws  basketball player makes in an individual woukout. As well as the percentage for all 3 and overall. Givens are there will be 50 shots at each range */
+
+alert("")
+
+
+
