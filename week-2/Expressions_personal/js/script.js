fbee990071ef388928a5ed00fefa539e317ca116
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-09 15:26:51 -0400

    expresessions personal commit 21
    I added prompts that I noticed I for to use.
    I also decided to add an alert so the user can see the total amount of
    the purchase when completed.

--- week-2/Expressions_personal/js/script.js
+++ week-2/Expressions_personal/js/script.js
@@ -4,19 +4,32 @@
 
 // variables and constants.;
 
-var tiresPurchased = 8;      // 2 pairs of tires for for sets of wheels.; 
-var tubesPurchased = 20;     // plenty of tubes never know whe you will need them.; 
+var tireSize = ("What size tires do you need? example 24x540 or 25x559."); 
+
+var tiresPurchased = prompt("How many tires do you need?");      // expect an even number as input from user;
+
+var tireSize = prompt("What size tires do you need? example 25x540 or 25x559.") // IMPORTANT! so user gets the right equipment.;
+
+var tubesPurchased = prompt ("How many tubes do you need?") ;     // this input could be higher than the numer of the tires purchased.;
+
+var tubeSize = prompt("What size tubes do you need? example 24x1 or 25x1 if you do not know just put the tire size you ordered.") // this in put should match the tire size or look like this ABxC.;
+
 const tiresCost = 30;        // tire cost from manufacturer in Georgia.;
+
 const tubesCost = 2.25;      // tube cost from manufacturer in Georgia.;
 
 // figure the cost for a year;
 
 var totalCostTubes = tubesPurchased * tubesCost;    // cost should be 45;
+
 var totalCostTires = tiresCost * tiresPurchased;    // cost should be 240; 
+
 var totalCost = totalCostTires + totalCostTubes;    // cost should be 285;
 
 // solution print out;
 
-console.log("You purchased " + tiresPurchased + " wheelchair tires for total cost of $" + totalCostTires + ". You also purchased " + tubesPurchased + " tubes for a total cost of $" + totalCostTubes + ". The total cost of all purchases is $" + totalCost + "."); 
+alert("You purchased " + tiresPurchased + " wheelchair tires size " + tireSize + " for total cost of $" + totalCostTires + ". You also purchased " + tubesPurchased + " tubes size " + tubeSize +  " for a total cost of $" + totalCostTubes + ". The total cost of all purchases is $" + totalCost + "."); 
+
+console.log("You purchased " + tiresPurchased + " wheelchair tires size " + tireSize + " for total cost of $" + totalCostTires + ". You also purchased " + tubesPurchased + " tubes size " + tubeSize +  " for a total cost of $" + totalCostTubes + ". The total cost of all purchases is $" + totalCost + "."); 
 
 
