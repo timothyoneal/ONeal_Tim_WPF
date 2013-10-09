d624d5c6e0037bd3ee427b7373686ee559063cc2
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-09 10:02:52 -0400

    expressions_personal description
    Added project  description. This project will figure how much I spend
    yearly maintaining the wheels for my  wheelchairs. Givens are Tires
    cost $30 ea and tubes.

--- week-2/Expressions_personal/js/script.js
+++ week-2/Expressions_personal/js/script.js
@@ -1,3 +1,6 @@
 // Tim ONeal Epressions_personal October 9, 2013;
 
 alert ("test");
+
+/*This project will figure how much I spend yearly maintaining the wheels for my  wheelchairs. Givens are Tires cost $30 ea and tubes.*/
+
