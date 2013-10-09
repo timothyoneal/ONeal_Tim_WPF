7293acdf8a931859e8ecbe4f66d7d0fdaa275f39
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-09 10:21:36 -0400

    expressions personal (commit 7)
    constants added
    const tiresCost= 30;
    const tubesCost= 2.25;

--- week-2/Expressions_personal/js/script.js
+++ week-2/Expressions_personal/js/script.js
@@ -2,9 +2,12 @@
 
 alert ("test");
 
-/*This project will figure how much I spend yearly maintaining the wheels for my  wheelchairs. Givens are Tires cost $30 ea and tubes.*/
+/*This project will figure how much I spend yearly maintaining the wheels for my  wheelchairs. Givens are Tires cost $30 ea and tubes cost 2.25 ea.*/
 
 // variables and constants.;
 
 var tiresPurchased= 7;
 var tubesPurchased= 20;
+const tiresCost= 30;
+const tubesCost= 2.25;
+
