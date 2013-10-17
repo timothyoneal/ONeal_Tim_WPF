5660eddbc2a01ef698e939f058edd34fdc454600
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 17:31:59 -0400

    wacky commit 10
    added variable
    var timePerMile= .16666666666667;

--- week-3/conditionals_wacky/js/script.js
+++ week-3/conditionals_wacky/js/script.js
@@ -2,8 +2,9 @@
 
 alert("test");
 
-/*A wheelchair user can push hs chair 5 miles in one hour. The user has an appointment in one hour will they need a ride or can they make it in time pushing.*/
+/*A wheelchair user can push hs chair 6 miles in one hour. The user has an appointment in one hour will they need a ride or can they make it in time pushing.*/
 
-var wheelchairSpeed=5;      // this is estimated speed.;
+var wheelchairSpeed=6;      // this is estimated speed.;
 var trvelDistance=prompt("How far away do you have to travel?");        
 var timeToAppt=1;           // 1 hour untill appointment.;
+var timePerMile= .16666666666667;              
