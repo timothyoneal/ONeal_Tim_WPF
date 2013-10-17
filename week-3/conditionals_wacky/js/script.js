2d76f91da71506c7b9d14e7bf0374779ab9f7b7b
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 16:52:40 -0400

    wacky commit 4
    added variable
    var wheelchairSpeed=6;

--- week-3/conditionals_wacky/js/script.js
+++ week-3/conditionals_wacky/js/script.js
@@ -1,3 +1,7 @@
 // Tim ONeal conditional wacky October 17, 2013;
 
-alert("test"); 
+alert("test");
+
+/*A wheelchair user can push hs chair 6 miles in one hour. The user has an appointment in one hour will they need a ride or can they make it in time pushing.*/
+
+var wheelchairSpeed=6; 
