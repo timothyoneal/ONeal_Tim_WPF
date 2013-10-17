0d5757607294582b5fc8afd4e36c3708ac571e6a
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 11:47:20 -0400

    personal commit 21
    cleaned up spelling errors and removed alert

--- week-3/conditionals_personal/js/script.js
+++ week-3/conditionals_personal/js/script.js
@@ -1,14 +1,14 @@
 // Tim ONeal conitional personal October 17 2013;
 
-alert("test");
+
 
 /*The National Wheelchair Basketball Association has a requirement of 10 games played to be eligible for the national tournament. This project will tell teams if they have met the minimum required games.*/
 
 // calculate number of team games.;
 
-var gamesWon=prompt("Enter the number of games the team has won.");      // in reality this will be enter wher team rep reports the team scores to the national office;
+var gamesWon=prompt("Enter the number of games the team has won.");      // in reality this will be entered when team rep reports the team scores to the national office;
 
-var gamesLost=prompt("Enter the number of games the team has lost.");    // in reality this will also be enter wher team rep reports the team scores to the national office;
+var gamesLost=prompt("Enter the number of games the team has lost.");    // in reality this will also be entered when team rep reports the team scores to the national office;
 
 var totalGames=gamesLost+gamesWon; 
 
