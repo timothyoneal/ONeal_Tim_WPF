34fb06b2ae0e11d7d40f8e061d6d6049fcb498d6
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 17:44:33 -0400

    wacky commit 13
    added if conditon
    if (travelDistance*timePerMile<timeToAppt) {
    
    }

--- week-3/conditionals_wacky/js/script.js
+++ week-3/conditionals_wacky/js/script.js
@@ -5,10 +5,12 @@ alert("test");
 /*A wheelchair user can push hs chair 6 miles in one hour. The user has an appointment in one hour will they need a ride or can they make it in time pushing.*/
 
 var wheelchairSpeed=6;      // this is estimated speed.;
-var trvelDistance=prompt("How far away do you have to travel?");        
+var travelDistance=prompt("How far away do you have to travel?");        
 var timeToAppt=1;           // 1 hour untill appointment.;
-var timePerMile= .16666666666667;   // 10 min mile/60 min in 1 hour.;
+var timePerMile=.16666666666667;   // 10 min mile/60 min in 1 hour.;
 
 // can the user make the trip in the time frame?;
 
+if (travelDistance*timePerMile<timeToAppt) {
 
+}
