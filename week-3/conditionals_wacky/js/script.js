8e1940f45f760d0a5491390c623b90fbef306a04
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 17:39:55 -0400

    wacky commit 12
    added comment
    // can the user make the trip in the time frame?;

--- week-3/conditionals_wacky/js/script.js
+++ week-3/conditionals_wacky/js/script.js
@@ -8,3 +8,7 @@ var wheelchairSpeed=6;      // this is estimated speed.;
 var trvelDistance=prompt("How far away do you have to travel?");        
 var timeToAppt=1;           // 1 hour untill appointment.;
 var timePerMile= .16666666666667;   // 10 min mile/60 min in 1 hour.;
+
+// can the user make the trip in the time frame?;
+
+
