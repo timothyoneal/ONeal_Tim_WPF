93f99db506412d35e2eab2d40c7c2cf890e14147
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 10:10:48 -0400

    conditonal personal commit 4
    added comment /*The National Wheelchair Basketball Association has a
    requirement of 10 games played to be eligible for the national
    tournament. This project will tell teams if they have met the minimum
    required games.*/

--- week-3/conditionals_personal/js/script.js
+++ week-3/conditionals_personal/js/script.js
@@ -1,3 +1,5 @@
 // Tim ONeal conitional personal October 17 2013;
 
 alert("test");
+
+/*The National Wheelchair Basketball Association has a requirement of 10 games played to be eligible for the national tournament. This project will tell teams if they have met the minimum required games.*/
