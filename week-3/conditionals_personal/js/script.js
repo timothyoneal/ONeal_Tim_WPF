4df0180ac1a204100bdebe58e9ed7bc484ce9b61
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 10:13:53 -0400

    conditional commit 5
    added comment // calculate number of team games.;

--- week-3/conditionals_personal/js/script.js
+++ week-3/conditionals_personal/js/script.js
@@ -3,3 +3,5 @@
 alert("test");
 
 /*The National Wheelchair Basketball Association has a requirement of 10 games played to be eligible for the national tournament. This project will tell teams if they have met the minimum required games.*/
+
+// calculate number of team games.;
