b122b2c62415f1b1ec48efafd0a3323e9f7266ce
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 19:31:01 -0400

    industry commit 10
    changed line 11 to read
    const printerDPI=600;       //printer prints at 600 dpi;

--- week-3/conditionals_industry/js/script.js
+++ week-3/conditionals_industry/js/script.js
@@ -8,6 +8,7 @@ var bannerWidth=prompt("What is the banner width in pixels?");      // input sho
 
 var bannerHeight=prompt("What is the banner height in pixels?");     // input should be a number.;
 
-const dpi=72;       //image created at 72 dpi;
+const printerDPI=600;       //printer prints at 600 dpi;
+
 
 
