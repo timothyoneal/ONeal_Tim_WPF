fee4054da8c92e33f24928d45b6869fb114930c5
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 19:17:47 -0400

    industry commit 6
    added variable
    var bannerHeight=prompt("What is the banner height in pixels?");

--- week-3/conditionals_industry/js/script.js
+++ week-3/conditionals_industry/js/script.js
@@ -5,3 +5,5 @@ alert("test");
 /*A client has a banner on their website the they would like to use in a print and campaign. They would like it to take up a full page in an upcoming magazine, we know the pages in the magazine are 8.5 in. by 11 in. Can the the banner be printed with out additional editing.*/
 
 var bannerWidth=prompt("What is the banner width in pixels?");
+
+var bannerHeight=prompt("What is the banner height in pixels?");    
