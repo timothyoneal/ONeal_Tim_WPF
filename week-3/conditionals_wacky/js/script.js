d47c7692b0043e2a919690fd8eabf36b59542bd6
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-17 18:17:05 -0400

    wacky commit 20
    changed if else to ternary and tested

--- week-3/conditionals_wacky/js/script.js
+++ week-3/conditionals_wacky/js/script.js
@@ -5,16 +5,13 @@ alert("test");
 /*A wheelchair user can push hs chair 6 miles in one hour. The user has an appointment in one hour will they need a ride or can they make it in time pushing.*/
 
 var wheelchairSpeed=6;      // this is estimated speed.;
-var travelDistance=prompt("How far away do you have to travel?");        
-var timeToAppt=prompt("What time do you need to arrive? please use hours.");           
+
+var travelDistance=prompt("How far away do you have to travel?");
+
+var timeToAppt=prompt("What time do you need to arrive? please use hours.");
+
 var timePerMile=.16666666666667;   // 10 min mile/60 min in 1 hour.;
 
 // can the user make the trip in the time frame?;
 
-if (travelDistance*timePerMile<timeToAppt) {
-    //trip can be made with in time frame.;
-    console.log("You can make the trip pushig yourself.");
-}else{
-    //trip can not be made.;
-    console.log("You can not make the trip pushig yourself.");
-}
+(travelDistance*timePerMile<timeToAppt)?console.log("You can make the trip pushig yourself."):console.log("You can not make the trip pushig yourself.");
