dd355526ca5d8dc5a4b59cb6583fa726c4d101aa
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-24 11:51:18 -0400

    funtion personal 4
    added comment
    
    /*I play wheelchair basketball the majority of our tournaments are 5
    games over 2 days this function will calculte the average points per
    game over 5 game tournament.*/

--- week-4/funtions_personal/js/script.js
+++ week-4/funtions_personal/js/script.js
@@ -1,3 +1,5 @@
 // Tim ONeal functions personal October 24, 2013;
 
 alert("test");
+
+/*I play wheelchair basketball the majority of our tournaments are 5 games over 2 days this function will calculte the average points per game over 5 game tournament.*/
