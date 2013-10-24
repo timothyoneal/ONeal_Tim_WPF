57a1c57e9bbe9a1675afbdabbea582f69d64c4db
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-24 11:54:49 -0400

    function personal 5
    added comment
    
    // calculate points per game.;

--- week-4/funtions_personal/js/script.js
+++ week-4/funtions_personal/js/script.js
@@ -3,3 +3,5 @@
 alert("test");
 
 /*I play wheelchair basketball the majority of our tournaments are 5 games over 2 days this function will calculte the average points per game over 5 game tournament.*/
+
+// calculate points per game.;
