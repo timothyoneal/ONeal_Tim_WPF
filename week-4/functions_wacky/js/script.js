de64bb19198f950685523b7c24067d94c796e824
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-24 18:55:32 -0400

    function wacky 11
    added varible
    var game7=prompt("how many points did you score in game 7?");

--- week-4/functions_wacky/js/script.js
+++ week-4/functions_wacky/js/script.js
@@ -10,3 +10,4 @@ var game3=prompt("how many points did you score in game 3?");
 var game4=prompt("how many points did you score in game 4?");
 var game5=prompt("how many points did you score in game 5?");
 var game6=prompt("how many points did you score in game 6?");
+var game7=prompt("how many points did you score in game 7?");
