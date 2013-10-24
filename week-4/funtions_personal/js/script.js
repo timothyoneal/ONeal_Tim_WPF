d29e16ec66ef63d4ca22a937b0856b903c488de9
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-24 12:01:34 -0400

    function personal 6
    added variable
    
    var game1=prompt("How many points did you score in game 1?");

--- week-4/funtions_personal/js/script.js
+++ week-4/funtions_personal/js/script.js
@@ -5,3 +5,5 @@ alert("test");
 /*I play wheelchair basketball the majority of our tournaments are 5 games over 2 days this function will calculte the average points per game over 5 game tournament.*/
 
 // calculate points per game.;
+
+var game1=prompt("How many points did you score in game 1?");
