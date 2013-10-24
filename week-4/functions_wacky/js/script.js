efb860b2f6cdcf4190b745736bd2843855bec576
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-24 18:29:36 -0400

    function wacky 4
    added comment
    
    /*In addition to tracking points per game some teams track player
    progress from tournament to tournament. this will make those
    calculations easier.*/

--- week-4/functions_wacky/js/script.js
+++ week-4/functions_wacky/js/script.js
@@ -1,3 +1,5 @@
 // Tim ONeal functions wacky October 24, 2013;
 
 alert("test");
+
+/*In addition to tracking points per game some teams track player progress from tournament to tournament. this will make those calculations easier.*/
