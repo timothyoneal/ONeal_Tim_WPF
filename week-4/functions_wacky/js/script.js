3800168ddfac278dbd0ed9941606c6c96adf94ef
timothyoneal <tjoneal@Timothy-ONeals-MacBook-Pro.local>
2013-10-24 18:37:13 -0400

    function wacky5
    added variable
    
    var game1=prompt("how many points did you score in game 1?");

--- week-4/functions_wacky/js/script.js
+++ week-4/functions_wacky/js/script.js
@@ -3,3 +3,5 @@
 alert("test");
 
 /*In addition to tracking points per game some teams track player progress from tournament to tournament. this will make those calculations easier.*/
+
+var game1=prompt("how many points did you score in game 1?");
